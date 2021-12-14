d3449f578149b089ca22e2be626399a90abcd5ab
Marcus Rundén <marcus.runden@hotmail.com>
2021-12-14 15:53:22 +0100

    Testat lite i Marcus-Scenen

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -8,6 +8,7 @@ public class SoundManager : MonoBehaviour
     [SerializeField] Slider volumeSlider;
     void Start()
     {
+        //Checks if there are volume sliders value saved from before and loads them. If there is no saved value, sets volume to 100%
         if(!PlayerPrefs.HasKey("Volume"))
         {
             PlayerPrefs.SetFloat("Volume", 1);
@@ -25,10 +26,12 @@ public class SoundManager : MonoBehaviour
     }
     private void Load()
     {
+        //Loads the volume sliders value from last session
         volumeSlider.value = PlayerPrefs.GetFloat("Volume");
     }
     private void Save()
     {
+        //Saves the value of the volume slider between different play sessions
         PlayerPrefs.SetFloat("Volume", volumeSlider.value);
     }
 }
