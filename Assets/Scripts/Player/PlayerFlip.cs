ce5b4aa1da8153d5899fbff775e00ac447d700eb
Codatron <87000871+Codatron@users.noreply.github.com>
2021-12-14 13:54:59 +0100

    Bomb flip

--- Assets/Scripts/Player/PlayerFlip.cs
+++ Assets/Scripts/Player/PlayerFlip.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class PlayerFlip : MonoBehaviour
 {
     GameObject gun;
+    GameObject bomb;
     Gun refGun;
+    PickUpBomb refPickUpBomb;
     Transform firePoint;
     SpriteRenderer playerSpriteRenderer;
     SpriteRenderer gunSpriteRenderer;
+    SpriteRenderer bombSpriteRenderer;
     public Sprite playerSpriteBack;
     public Sprite playerSpriteFront;
     bool isOverY;
@@ -19,8 +22,12 @@ public class PlayerFlip : MonoBehaviour
         gun = GameObject.FindGameObjectWithTag("Gun");
         refGun = gun.GetComponent<Gun>();
 
+        bomb = GameObject.FindGameObjectWithTag("BombHands");
+        refPickUpBomb = bomb.GetComponent<PickUpBomb>();
+
         playerSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         gunSpriteRenderer = gun.GetComponent<SpriteRenderer>();
+        bombSpriteRenderer = bomb.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -31,6 +38,14 @@ public class PlayerFlip : MonoBehaviour
         isOverY = refGun.firePoint.position.y > gameObject.transform.position.y;
         playerSpriteRenderer.sprite = isOverY ? playerSpriteBack : playerSpriteFront;
         gunSpriteRenderer.sortingOrder = isOverY ? 0 : 2;
+
+
+        bombSpriteRenderer.flipX = refGun.firePoint.position.x > gameObject.transform.position.x;
+        isOverY = refGun.firePoint.position.y > gameObject.transform.position.y;
+        bombSpriteRenderer.sortingOrder = isOverY ? 0 : 2;
+        bombSpriteRenderer.flipX = isOverY ? true : false;
+        // TODO : Program the bomb pacement so that it is in accordance with the directionthe player is facing and also on which side of the body that the bom is on at that particular time. 14/12/2021
+
 
         //if (isOverY)
         //{
