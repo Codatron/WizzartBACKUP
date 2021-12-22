138051fff6aafe2d0562a0b020aad7209ccd63b9
mayawingardh <mayaandersson@hotmail.se>
2021-12-22 16:06:48 +0100

    håll tummarna

--- Assets/CameraShake.cs
+++ Assets/CameraShake.cs
@@ -6,7 +6,6 @@ public class CameraShake : MonoBehaviour
 {
        public IEnumerator Shake(float duration, float magnitut)
     {
-
         Vector3 originalPos = transform.localPosition;
         float elaped = 0.0f;
 
--- Assets/Scripts/Weapons/PickUpBomb.cs
+++ Assets/Scripts/Weapons/PickUpBomb.cs
@@ -9,16 +9,16 @@ public class PickUpBomb : MonoBehaviour
     public GameObject bombFactory;
     public GameObject dropBombPos;
     public GameObject factory_1;
+    public GameObject boom;
     public Sprite factory_2;
     public GameObject clone;
     public CameraShake cameraShake;
-
+    public SpawnBomb refSpawnBomb;
+    public GameObject boomPlace;
 
 
     private BoolKeeper refBoolKeeper;
     public int bombCounter = 0;
-
-    public SpawnBomb refSpawnBomb;
 
     private void Start()
     {
@@ -28,29 +28,38 @@ public class PickUpBomb : MonoBehaviour
         refBoolKeeper = g.GetComponent<BoolKeeper>();
     }
 
-    private void Update()
-    {
-        
-    }
-
     public void ChangeFactory()
     {
         if (bombCounter == 1)
-        {
-            Debug.Log("Hej");
-            factory_1.GetComponent<SpriteRenderer>().sprite = factory_2;
+        {          
+            Boom();
 
+            StartCoroutine(cameraShake.Shake(.15f, .4f));
+            factory_1.GetComponent<SpriteRenderer>().sprite = factory_2;
         }
 
         if (bombCounter == 2)
         {
-            //TODO EXPOASITION
+            Boom();
+            factory_1.GetComponent<SpriteRenderer>().sprite = factory_2;//ANDRA HAR
         }
 
         if (bombCounter == 3)
         {
-            //TODO EXPOASITION
+            GameObject boomClone = Instantiate(boom, boomPlace.transform.position, Quaternion.identity); //Byt bild har
+            Destroy(boomClone, 0.8f);
+
+            StartCoroutine(cameraShake.Shake(.15f, .4f));
+            factory_1.GetComponent<SpriteRenderer>().sprite = factory_2;//ANDRA HAR
         }
+    }
+
+    private void Boom()
+    {
+        GameObject boomClone = Instantiate(boom, boomPlace.transform.position, Quaternion.identity);
+        Destroy(boomClone, 0.8f);
+
+        StartCoroutine(cameraShake.Shake(.15f, .4f));
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -71,7 +80,6 @@ public class PickUpBomb : MonoBehaviour
 
             bombExposition();
 
-
             refBoolKeeper.dontShoot = false;
 
             bombCounter++;
@@ -84,8 +92,10 @@ public class PickUpBomb : MonoBehaviour
     private void bombExposition()
     {
         Destroy(clone,2);
-        StartCoroutine(cameraShake.Shake(.15f, .4f));
+       
         Invoke(nameof(ChangeFactory), 2);
     }
+
+    //TODO NY SPRITE FOR BOMB MAN INSTANSIATAR VID FABRIKEN
 }
 
