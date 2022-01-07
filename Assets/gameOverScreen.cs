c43012c4100d52a53daee3581233c328a169cbbb
mayawingardh <mayaandersson@hotmail.se>
2022-01-07 14:29:28 +0100

    fix

---
+++ Assets/gameOverScreen.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameOverScreen : MonoBehaviour
+{
+    public void Restart()
+    {      
+       SceneManager.LoadScene(sceneBuildIndex: 2);
+       MusicSound.PlayBossMusic();  
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
